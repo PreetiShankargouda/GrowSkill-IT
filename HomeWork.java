80b4a46d85074ec92c294ee9ed95a74e7018bfdf
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-06 13:40:44 +0530

    Add files via upload

---
+++ HomeWork.java
@@ -0,0 +1,26 @@
+package myfirstpackage;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int M = 80;
+        int S = 90;
+        int E = 95;
+        
+        
+        double total = 265;
+        int n = 300;
+        
+        System.out.println("Maths marks is "+ M);
+        System.out.println("Science marks is "+ S);
+        System.out.println("English marks is "+ E);
+        
+        System.out.println("Total marks are "+ (M+S+E));
+    
+        
+        System.out.println("Average "+ total/n*100);
+        
+	}
+
+}
