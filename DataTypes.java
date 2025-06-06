13747e94529acf0ebd7189144de57744c89b7268
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-06 13:41:57 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+package myfirstpackage;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 24;
+		float percentage = 84.48f;
+		char grade = 'A';
+		boolean isThisTrue = true;
+		
+		System.out.println("age is "+ age);
+		System.out.println("percentage is "+ percentage);
+		System.out.println("grade is "+ grade);
+		System.out.println("boolean is "+ isThisTrue);
+
+	}
+
+}
