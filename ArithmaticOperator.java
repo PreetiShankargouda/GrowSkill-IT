916bd9bc3dc2fac0ee137311d24526eb1d5861e1
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-06 13:43:13 +0530

    Add files via upload

---
+++ ArithmaticOperator.java
@@ -0,0 +1,20 @@
+package myfirstpackage;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 50;
+		int b = 25;
+		
+		System.out.println("addition is "+ (a+b));
+		System.out.println("Substartion is "+ (a-b));
+		System.out.println("Multiply is "+ (a*b));
+		System.out.println("Division is "+ (a/b));
+		System.out.println("addition is "+ (a+b));
+		System.out.println("Reminder is "+ (a%b));
+
+	}
+
+}
