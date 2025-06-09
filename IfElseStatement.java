2193828f12ce1bc214a7bce6f089eba1e74d815d
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-09 18:43:31 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,20 @@
+package conditionalstatements;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 15;
+		if(age>=18)
+		{
+			System.out.println("Adult");
+		
+		}
+		else
+		{
+			System.out.println("Child");
+		}
+
+	}
+
+}
