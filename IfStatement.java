4ca48c06606ad571ff5f5ab3aa4901e90dd022a6
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-09 18:43:13 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,17 @@
+package conditionalstatements;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 17;
+		
+		if(age>=18)
+		
+		    System.out.println("adult");
+		    System.out.println("How are you");
+	
+		    
+	}
+
+}
