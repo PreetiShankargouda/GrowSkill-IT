95ab9ca85418d0c369c2a9a5ffe6bbfbe166d0a6
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-09 18:44:06 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,22 @@
+package conditionalstatements;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Month = 3;
+		switch (Month)
+		{
+		 case 1: System.out.println("Jan"); break;
+		 case 2: System.out.println("Feb"); break;
+		 case 3: System.out.println("Mar"); break;
+		 case 4: System.out.println("Apr"); break;
+		 case 5: System.out.println("May"); break;
+		 default : System.out.println("Invalid Input"); break;
+		 
+		
+		}
+
+	}
+
+}
