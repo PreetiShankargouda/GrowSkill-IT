8bf0f8686eb4845a05ad99220efc461f6cfbf2f3
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-10 12:04:03 +0530

    Add files via upload

---
+++ SumofNumber.java
@@ -0,0 +1,15 @@
+package loopingstatement;
+
+public class SumofNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i=1;i<=100;i++)
+		{
+			sum = sum+i;
+		}
+		System.out.println("sum is " +sum);
+	}
+
+}
