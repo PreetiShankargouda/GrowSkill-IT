5fd6c8dc230a676a13d05c0c3019042514b77f40
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-11 18:46:50 +0530

    Add files via upload

--- SumofNumber.java
+++ SumofNumber.java
@@ -5,7 +5,7 @@ public class SumofNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		for (int i=1;i<=100;i++)
+		for (int i=1;i<=4;i++)
 		{
 			sum = sum+i;
 		}
