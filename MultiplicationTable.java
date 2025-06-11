eb66e4b862417613cb25673efd3610543ff8888f
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-11 18:46:18 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,17 @@
+package loopingstatement;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=70;i++)
+		{
+			if(i%7==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
