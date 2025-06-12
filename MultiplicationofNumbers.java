8fa88dec33ce29510a303bb222a68377db535b0f
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-12 14:33:39 +0530

    Add files via upload

---
+++ MultiplicationofNumbers.java
@@ -0,0 +1,44 @@
+package loopingstatement;
+
+public class MultiplicationofNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=10;i++)
+		{
+			if(i%1==0)
+			{
+				System.out.println(i);
+			}
+		}
+		for (int j=1;j<=20;j++)
+		{
+			if(j%2==0)
+			{
+				System.out.println(j);
+			}
+		}
+		for (int k=1;k<=30;k++)
+		{
+			if(k%3==0)
+			{
+				System.out.println(k);
+			}
+		}
+		for (int p=1;p<=40;p++)
+		{
+			if(p%4==0)
+			{
+				System.out.println(p);
+			}
+		}
+		for (int v=1;v<=50;v++)
+		{
+			if(v%5==0)
+			{
+				System.out.println(v);
+			}
+		}
+	}
+
+}
