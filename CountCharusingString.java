77cda3c072f53b8c0e74b05a7f9f88986ade058c
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-18 13:27:20 +0530

    Add files via upload

---
+++ CountCharusingString.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class CountCharusingString {
+
+	public static void main(String[] args) {
+		String str = "preeti";
+		
+		int totalchar = str.length();
+			
+		System.out.println("String name is : " +str);
+		System.out.println("Number of count in string :" + totalchar);
+
+	}
+
+}
