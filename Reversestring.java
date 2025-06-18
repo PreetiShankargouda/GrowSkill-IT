4daf5fff6b9c8870f77d7b384b3d0994bea315a1
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-18 13:27:03 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		String str = "Java";
+		String rev = "";
+		
+		for (int i = str.length()-1; i>=0; i--)
+		{
+			rev = rev + str.charAt(i);
+			
+		}
+		System.out.println(rev);
+	}
+
+}
