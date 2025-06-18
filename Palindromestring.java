b29205e67dfa6ec39383ba85c6307aa2124c8809
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-18 13:27:35 +0530

    Add files via upload

---
+++ Palindromestring.java
@@ -0,0 +1,26 @@
+package strings;
+
+public class Palindromestring {
+
+	public static void main(String[] args) {
+		String str = "madam";
+		String rev = "";
+		
+		for (int i = str.length()-1; i>=0; i--)
+		{
+			rev = rev + str.charAt(i);
+		}
+		if (str.equals(rev))
+		{
+			System.out.println("true");
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		
+	
+	}
+
+}
