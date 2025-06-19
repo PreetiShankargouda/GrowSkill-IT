64e137bed6b50560db75e1b4942b0010951f5eca
Preeti Shankargouda <92423912+PreetiShankargouda@users.noreply.github.com>
2025-06-19 12:24:29 +0530

    Add files via upload

---
+++ Uppercasestring.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Uppercasestring {
+
+	public static void main(String[] args) {
+		String str = "java is fun";
+		
+		String S1 = str.toUpperCase();
+		
+		System.out.println(S1);
+		
+		
+
+	}
+
+}
